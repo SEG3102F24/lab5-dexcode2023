c138b1b097a299acf04e590ad6d62153eb4c700d
dexcode2023 <dextreday@gmail.com>
2024-10-18 20:58:37 -0400

    completing lab

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Observable} from "rxjs";
-import {Employee} from "../model/employee";
+import { BehaviorSubject, Observable } from "rxjs";
+import { Employee } from "../model/employee";
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +10,42 @@ import {Employee} from "../model/employee";
 export class EmployeeService {
   employees$: BehaviorSubject<readonly Employee[]> = new BehaviorSubject<readonly Employee[]>([]);
 
+  constructor(private firestore: Firestore) {
+    this.getEmployeesFromFirestore(); // Fetch employees when service is initialized
+  }
+
   get $(): Observable<readonly Employee[]> {
     return this.employees$;
   }
 
-  addEmployee(employee: Employee) {
-    this.employees$.next([...this.employees$.getValue(), employee]);
-    return true;
+  // Add employee to Firestore and update local state
+  async addEmployee(employee: Employee) {
+    try {
+      const employeeCollection = collection(this.firestore, 'employees');
+      await addDoc(employeeCollection, { ...employee }); // Add to Firestore
+      this.employees$.next([...this.employees$.getValue(), employee]); // Update local state
+      return true;
+    } catch (error) {
+      console.error("Error adding employee to Firestore: ", error);
+      return false;
+    }
   }
+
+  // Retrieve employees from Firestore and update local state
+  getEmployeesFromFirestore() {
+    const employeeCollection = collection(this.firestore, 'employees');
+    collectionData(employeeCollection, { idField: 'id' })
+      .pipe(
+        map((data) => {
+          return data.map((emp) => ({
+            ...emp,
+            dateOfBirth: emp['dateOfBirth'].toDate ? emp['dateOfBirth'].toDate() : new Date(emp['dateOfBirth'])
+          })) as Employee[];
+        })
+      )
+      .subscribe((employees: Employee[]) => {
+        this.employees$.next(employees); // Update local state with Firestore data
+      });
+  }
+  
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDkM__XsDyk2WGGc94z67lxtHULPZ9F-9M",
+        authDomain: "lab-5-dexcode2023.firebaseapp.com",
+        projectId: "lab-5-dexcode2023",
+        storageBucket: "lab-5-dexcode2023.appspot.com",
+        messagingSenderId: "205841522356",
+        appId: "1:205841522356:web:d61419a63017b70c18c1e1",
+        measurementId: "G-XY17034T4W"
+
+
+    }
+
+};
